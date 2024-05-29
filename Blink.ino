e1b54f39e20e2cb359453c398b1f97c745ceb3ec
Shriya-Shravan-Sareen <shriyasareen2008@gmail.com>
2024-05-29 14:38:40 +0530

    Blink.ino

--- Blink.ino
+++ Blink.ino
@@ -3,8 +3,8 @@ void setup() {
   pinMode(LED_BUILTIN, OUTPUT);
 }
 
-// the loop function runs over and over again forever
 void loop() {
+  // the loop function runs over and over again forever
   digitalWrite(LED_BUILTIN, HIGH);  // turn the LED on 
   delay(1000);                      // wait for a second
   digitalWrite(LED_BUILTIN, LOW);   // turn the LED off 
